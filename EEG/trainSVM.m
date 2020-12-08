9443af3064f64f6f4b40a6492006a9a253803f81
unspection <69597762+unspection@users.noreply.github.com>
2020-12-08 23:10:36 +0100

    Add files via upload

---
+++ EEG/trainSVM.m
@@ -0,0 +1,25 @@
+function SVMModels = trainSVM(feature, target, kernel)
+% Function from: https://github.com/fennialesmana/sleep-stage-identification
+
+%Train Support Vector Machines (SVM) model using One Vs One
+%   Syntax:
+%   SVMModels = trainSVM(feature, target, kernel)
+%
+%   Input:
+%   *) feature - feature collection
+%      (Matrix size: total samples X total features)
+%   *) target  - target of each sample
+%      (Matrix size: total samples X 1)
+%   *) kernel  - kernel used for SVM
+%
+%   Output:
+%   *) SVMModels - SVM model collection
+%      (Cell size: totalClass * (totalClass - 1) / 2)
+
+    if nargin == 2
+        kernel = 'linear';
+    end
+    
+    t = templateSVM('KernelFunction', kernel);
+    SVMModels = fitcecoc(feature, target, 'Learners', t); %fit multiclass models for SVM
+end
