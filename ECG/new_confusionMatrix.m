5b46016070b41582aee0ed2dc9fd735c00e62d53
unspection <69597762+unspection@users.noreply.github.com>
2020-12-17 15:40:32 +0100

    Added save pic function

--- ECG/new_confusionMatrix.m
+++ ECG/new_confusionMatrix.m
@@ -3,4 +3,6 @@ function new_confusionMatrix(knownStages, predictedStages)
 C = confusionchart(knownStages, predictedStages, 'RowSummary','row-normalized','ColumnSummary','column-normalized');
 C.Title = sprintf('Confusion matrix');
 
+saveas(C, 'NEWconfusionMatrix.png')
+
 end
